8805ed24890f68900cd8e9b53f4938749786f98f
aalshukri <aalshukri@googlemail.com>
2021-01-17 16:16:34 +0000

    Extremely basic NLP.

--- script.js
+++ script.js
@@ -1,5 +1,30 @@
 /* myfunction() */
-function myFunction() {
-    var x = document.getElementById("input").value;
-    alert("Suggesting... "+x);
-  }
+function myFunction()
+{
+    let userInput = document.getElementById("input").value;
+    console.log("Suggesting... "+userInput);
+    generateSuggestions(userInput);
+}
+
+/* generateSuggestions() */
+function generateSuggestions(userInput)
+{
+    console.log("generateSuggestions "+userInput);
+
+    //let doc = nlp(userInput);
+
+    //you can see the text has no tags
+    //console.log(doc.has('#Noun')) //false
+  
+    //but the whole api still works
+    //console.log(doc.has('my .* is .? named /^b[oa]rt/')) //true    
+
+
+    let doc = nlp.tokenize(userInput);
+
+    console.log(doc);
+    console.log( (doc.out('tags')[0]) );
+
+    let arr = doc.json().map(o=> o.text)
+    console.log(arr);
+}  
